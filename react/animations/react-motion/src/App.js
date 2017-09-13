d4889f9aa6257725341773a2e5ee6c0f79178222
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-09-13 16:57:34 -0600

    list expanding works

--- react/animations/react-motion/src/App.js
+++ react/animations/react-motion/src/App.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import { TransitionMotion, spring, presets } from 'react-motion';
+import { TransitionMotion, Motion, spring, presets } from 'react-motion';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       todo: [
-        {key: 't1', data: {item: 'learn this library', checked: false}, style: {height: 0, opacity: 1}},
-        {key: 't2', data: {item: 'go to Europe', checked: false}, style: {height: 0, opacity: 1}},
-        {key: 't3', data: {item: 'pack', checked: false}, style: {height: 0, opacity: 1}},
-        {key: 't4', data: {item: 'take my plants to my parents', checked: false}, style: {height: 0, opacity: 1}},
-        {key: 't5', data: {item: 'do all the things', checked: false}, style: {height: 0, opacity: 1}},
-        {key: 't6', data: {item: "don't freak out about flying", checked: false}, style: {height: 0, opacity: 1}}
-      ]
+        {key: 't1', data: {item: 'learn this library', checked: false}},
+        {key: 't2', data: {item: 'go to Europe', checked: false}},
+        {key: 't3', data: {item: 'pack', checked: false}},
+        {key: 't4', data: {item: 'take my plants to my parents', checked: false}},
+        {key: 't5', data: {item: 'do all the things', checked: false}},
+        {key: 't6', data: {item: "don't freak out about flying", checked: false}}
+      ],
+      open: false
     }
   }
 
-  getStyles() {
-    const styles = this.state.todo.map(e => {
-      e.style= {height: spring(60, presets.gentle), opacity: spring(1, presets.gentle)};
-      return e
+  getStyles = () => {
+    return this.state.todo.map(e => {
+      return {...e, style:{height: spring(60, presets.gentle), opacity: spring(1, presets.gentle)}}
     })
-    console.log(styles)
-    return styles;
   }
 
-  getDefaults() {
-    console.log('default styles')
-    return this.state.todo.slice(0)
+  getDefaults = () => {
+    return this.state.todo.map(e=> ({...e, style:{height: 0, opacity: 0}}))
   }
 
   submit = (e) => {
@@ -61,11 +58,17 @@ class App extends Component {
     })
   }
 
-  willLeave() {
-    return {
-      height: spring(0, presets.gentle),
-      opacity: spring(0, presets.gentle)
-    }
+  toggleCheckbox = (index) => {
+    this.setState({
+      todo: this.state.todo.map((e, i) => {
+        if(i === index) e.data.checked = !e.data.checked
+        return e
+      }),
+    })
+  }
+
+  toggleInput = () => {
+    this.setState({open: !this.state.open})
   }
 
   willEnter() {
@@ -74,32 +77,47 @@ class App extends Component {
       opacity: 1
     }
   }
+  
+  willLeave() {
+    return {
+      height: spring(0),
+      opacity: spring(0)
+    }
+  }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.submit}>
-          <input ref={(input) => this.input = input}/>
-        </form>
+        <Motion defaultStyle={{x: 0}} style={{x: this.state.open ? spring(200) : spring(100)}}>
+          {style => {
+          return (
+            <form onSubmit={this.submit} style={{width: `${style.x}px`}}>
+              <input ref={(input) => this.input = input} onFocus={this.toggleInput} onBlur={this.toggleInput}
+              style={{width: `${style.x}px`}}/>
+            </form>
+          )}}
+        </Motion>
         <TransitionMotion 
           defaultStyles={this.getDefaults()}
           styles={this.getStyles()}
           willLeave={this.willLeave}
           willEnter={this.willEnter}>
-          {styles => (
+          {styles => {
+            return (
             <ul>
-              {styles.map(({key, data: {item, checked}, style}) => (
+              {
+                styles.map(({key, data: {item, checked}, style}, i) => (
                 <div key={key} style={{background: '#DDD', listStyleType: 'none', ...style}}>
-                  <li>
+                  <li className={checked ? 'todo-list completed' : 'todo-list'}>
                     {item}
-                  <button onClick={() => this.delete(key)}>delete</button>
+                    <button onClick={() => this.delete(key)}>delete</button>
+                    <input type='checkbox' onChange={() => this.toggleCheckbox(i)}/>
                   </li>
                 </div>
               ))}
             </ul>
-          )}
+          )}}
         </TransitionMotion>
-        
       </div>
     );
   }
